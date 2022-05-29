54360f4bd3a444df37ec7d1cf99d8bb086824ef7
Franco Castellitto <castellittofranco1@gmail.com>
2022-05-29 19:38:23 -0300

    Proyecto Inicializado

--- App.js
+++ App.js
@@ -1,20 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useRef, useState } from "react"
+import { StyleSheet, View, Button, TextInput, Alert, TouchableHighlight, Text, ScrollView } from "react-native"
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [task, setTask] = useState(undefined)
+    const [tasks, setTasks] = useState([])
+    const textInput = useRef()
+
+    const handleButton = () => {
+        if (task === "") return
+
+        setTasks([...tasks, task])
+        textInput.current.clear()
+        setTask(undefined)
+    }
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.inputContent}>
+                <TextInput ref={textInput} style={styles.textInput} value={task} placeholder="new task..." onChangeText={setTask} />
+                <Button style={styles.button} onPress={handleButton} title="Add" color="#880e4f" />
+            </View>
+
+            <ScrollView style={{ marginVertical: 5 }}>
+                {tasks.map((t, i) => {
+                    return (
+                        <TouchableHighlight
+                            activeOpacity={0.9}
+                            underlayColor="#560027"
+                            style={{
+                                padding: 5,
+                                margin: 5,
+                                backgroundColor: "#880e4f",
+                            }}
+                            onPress={() => Alert.alert(t)}
+                        >
+                            <Text
+                                style={{
+                                    color: "#ffffff",
+                                    fontWeight: "bold",
+                                }}
+                                key={i}
+                            >
+                                {t}
+                            </Text>
+                        </TouchableHighlight>
+                    )
+                })}
+            </ScrollView>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        marginHorizontal: 20,
+        marginTop: 60,
+    },
+    inputContent: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+    },
+    textInput: {
+        borderBottomWidth: 1,
+        borderBottomColor: "#880e4f",
+        padding: 5,
+        flex: 0.8,
+    },
+})
